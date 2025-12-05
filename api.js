33a276a88cdabe515b552dcb9b101690545eca9a
Rohan <rohansharmaxyz45@gmail.com>
2025-12-06 00:24:13 +0530

    Use environment variable for server port
    Updated server to use environment variable for port.

--- api.js
+++ api.js
@@ -23,6 +23,8 @@ app.get("/quotes", (req, res) => {
 });
 
 // Start Server
-app.listen(3000, () => {
-    console.log("API running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(API running on port ${PORT});
 });
